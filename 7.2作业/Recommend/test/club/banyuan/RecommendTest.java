7fd86e5abf3c3976a70ea06917921f9718112353
Zhu Yinsheng <yin11136@163.com>
2020-07-03 16:24:07 +0800

    集合

--- 7.2作业/Recommend/test/club/banyuan/RecommendTest.java
+++ 7.2作业/Recommend/test/club/banyuan/RecommendTest.java
@@ -1,5 +1,7 @@
 package club.banyuan;
 
+import java.util.LinkedList;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -21,11 +23,27 @@ public class RecommendTest {
     personalRecommender.addLikes("小水", "4");
 
     Assert.assertTrue(personalRecommender.likeBoth("小金", "2", "3"));
+    Assert.assertFalse(personalRecommender.likeBoth("小金", "2", "5"));
+
+    List<String> list = new LinkedList<>();
+    list.add("1");
+    list.add("2");
+    list.add("3");
+    list.add("4");
+    list.add("5");
+
+    Assert.assertTrue(list.containsAll(personalRecommender.recommendByPerson("小木")));
 
     Assert.assertEquals(5, personalRecommender.recommendByPerson("小木").size());
 
-    Assert.assertEquals(4, personalRecommender.recommendByProject("3").size());
+    List<String> list1 = new LinkedList<>();
+    list1.add("1");
+    list1.add("2");
+    list1.add("4");
+    list1.add("5");
 
+    Assert.assertTrue(list1.containsAll(personalRecommender.recommendByProject("3")));
+    System.out.println(personalRecommender.recommendByProject("3"));
+//    Assert.assertEquals(4, personalRecommender.recommendByProject("3").size());
   }
-
 }
