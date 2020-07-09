5bc2bdf1bf70508324be8059da0e33f70be463c2
Zhu Yinsheng <yin11136@163.com>
2020-07-09 10:21:45 +0800

    多线程2

--- 7.8作业/ShareCounter/src/club/banyuan/SharedCounter.java
+++ 7.8作业/ShareCounter/src/club/banyuan/SharedCounter.java
@@ -1,5 +1,13 @@
 package club.banyuan;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 public class SharedCounter extends Thread {
 
   static int counter = 0;
@@ -11,16 +19,38 @@ public class SharedCounter extends Thread {
   public static int increment(int numThreads, int numIncrementsPerThread)
       throws InterruptedException {
 
+//    for (int i = 0; i < numThreads; i++) {
+//      Thread thread = new Thread(new Runnable() {
+//        @Override
+//        public void run() {
+//          counter += numIncrementsPerThread;
+//        }
+//      });
+//      thread.start();
+//      thread.join();
+//    }
+//    return counter;
+//  }
+
+    ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
     for (int i = 0; i < numThreads; i++) {
-      Thread thread = new Thread(new Runnable() {
+      Future<Integer> future = executorService.submit(new Callable<Integer>() {
         @Override
-        public void run() {
-          counter += numIncrementsPerThread;
+        public Integer call() throws Exception {
+          sleep(50);
+          synchronized (SharedCounter.class) {
+            return counter += numIncrementsPerThread;
+          }
         }
       });
-      thread.start();
-      thread.join();
+      try {
+        future.get();
+      } catch (ExecutionException e) {
+        e.printStackTrace();
+      }
     }
+    executorService.shutdownNow();
+    executorService.awaitTermination(1, TimeUnit.SECONDS);
     return counter;
   }
 }
