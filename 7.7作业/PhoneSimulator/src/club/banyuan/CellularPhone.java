097ce0e5284ed02d20f6f1819a1143cb6b9233b0
Zhu Yinsheng <yin11136@163.com>
2020-07-09 08:29:24 +0800

    多线程2

--- 7.7作业/PhoneSimulator/src/club/banyuan/CellularPhone.java
+++ 7.7作业/PhoneSimulator/src/club/banyuan/CellularPhone.java
@@ -1,8 +1,5 @@
 package club.banyuan;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class CellularPhone extends Thread {
 
   private volatile boolean callInProgress = false;
